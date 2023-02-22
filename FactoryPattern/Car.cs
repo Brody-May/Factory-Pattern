e2a42ec6aee01e6bd7035650d74dc2dd3defd94e
Brody May <brodymay2019@outlook.com>
2023-02-22 14:22:10 -0500

    cars made

---
+++ FactoryPattern/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    internal class Car : IVehicle
+    {
+        public void Build()
+        {
+            Console.WriteLine("The Car is being put together");
+            Console.WriteLine("This could take a while....");
+        }
+    }
+
+
+}
---
+++ FactoryPattern/Motorcycle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    internal class Motorcycle : IVehicle
+    {
+        public void Build()
+        {
+            Console.WriteLine("The Motorcycle is being built.");
+            Console.WriteLine("Clink Clank");
+        }
+    }
+}
